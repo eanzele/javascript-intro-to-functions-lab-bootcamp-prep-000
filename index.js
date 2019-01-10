0a384d3303dc79180451bb47e86af466f7ccd3fd
Emmanuel Anzele <eanzele@gmail.com>
2019-01-10 14:39:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function shout(string) {
 }
 function whisper(string) {
   return string.toLowerCase ()
+<<<<<<< HEAD
 }
 function logShout(string) {
   return console.log(string.toUpperCase())
@@ -14,4 +15,6 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return "I can't hear you!";
  if (string === "I love you, Grandma.") return "I love you, too.";
 if (string === string.toUpperCase()) return "YES INDEED!";
+=======
+>>>>>>> d7f1c375d53a6fb3e4f53105633a8fdc19733410
 }
